704d9e2974add8198a8ab0358a17493fe515ed49
chaponanwill <chapowilliam09@gmail.com>
2024-12-15 02:32:41 -0500

    feature/bugs_corregir

--- src/components/ui/rainbow-button.tsx
+++ src/components/ui/rainbow-button.tsx
@@ -1,21 +1,25 @@
 import React from "react";
 import { cn } from "@/lib/utils";
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface RainbowButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: "light" | "dark"; // Propiedad personalizada opcional
+}
+
 export function RainbowButton({
   children,
   className,
+  variant = "light", // Valor predeterminado para la propiedad
   ...props
 }: RainbowButtonProps) {
   return (
     <button
       className={cn(
         "group relative inline-flex h-11 animate-rainbow cursor-pointer items-center justify-center rounded-xl border-0 bg-[length:200%] px-8 py-2 font-medium text-white text-xl transition-colors [background-clip:padding-box,border-box,border-box] [background-origin:border-box] [border:calc(0.08*1rem)_solid_transparent] focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
-        // light mode colors
-        "bg-[linear-gradient(#b8f3f3,#047060),linear-gradient(#121213_50%,rgba(18,18,19,0.6)_80%,rgba(18,18,19,0)),linear-gradient(90deg,hsl(var(--color-1)),hsl(var(--color-5)),hsl(var(--color-3)),hsl(var(--color-4)),hsl(var(--color-2)))]",
-        className,
+        variant === "light"
+          ? "bg-[linear-gradient(#b8f3f3,#047060),linear-gradient(#121213_50%,rgba(18,18,19,0.6)_80%,rgba(18,18,19,0)),linear-gradient(90deg,hsl(var(--color-1)),hsl(var(--color-5)),hsl(var(--color-3)),hsl(var(--color-4)),hsl(var(--color-2)))]"
+          : "bg-[linear-gradient(#4f4f4f,#000000),linear-gradient(#2b2b2b_50%,rgba(0,0,0,0.6)_80%,rgba(0,0,0,0)),linear-gradient(90deg,hsl(var(--color-5)),hsl(var(--color-2)),hsl(var(--color-3)),hsl(var(--color-4)),hsl(var(--color-1)))]",
+        className
       )}
       {...props}
     >
